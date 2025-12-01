bf619cdd224b87f58caf73c5e7d26d79a7cc6f44
Somya53 <somyasakalle@gmail.com>
2025-12-01 16:05:46 -0500

    this is frontend code

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,20 +1,19 @@
+import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+dotenv.config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
-};
-require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
-
-module.exports = {
-  solidity: "0.8.28",
+  defaultNetwork: "sepolia",
   networks: {
+    hardhat: {},
     sepolia: {
-      url: process.env.SEPOLIA_URL,
-      accounts: [process.env.PRIVATE_KEY]
-    }
-  }
+      url: process.env.SEPOLIA_RPC_URL || process.env.SEPOLIA_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
 };
 
 export default config;
